e035d739316074dcd9d6bb6d7be97015d9c7a88e
LEANDRINHO77 <leonardokaiodearaujosantanna@gmail.com>
2026-04-15 18:54:46 -0300

    backend

---
+++ backend/interface.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+app = tk.Tk()
+app.title("BUSSOLA DIGITAL")
+app.geometry("700x500")
+app.mainloop()
--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,20 @@
 import chatbot
-
+import os
 def start():
     print('Olá! Bem-vindo ao Bússola Digital')
     nome = input("Qual seu nome? ")
     
     print(f"\nMuito prazer, {nome}!")
-    print("O que gostaria de aprender hoje com seu guia digital?")
+    os.system("cls")
+    print("O que gostaria de aprender hoje com seu guia digital?\n")
+    
+    print("1- Segurunça digita")
+    print("2- cidadania online")
+    print("3- Prevenção de incidentes digitaos")
+    print("4- Criação de senhas seguras \n")
+    escolha = input("Digite o numero do topico: ")
 
+    
     while True:
         texto_usuario = input("\nVocê: ")
 
@@ -19,6 +27,7 @@ def start():
 
         resposta = chatbot.buscandoconhecimento(nome, texto_usuario)
         print(f"Chatbot: {resposta}")
+        print(f"\nMais alguma dúvida?")
 
 if __name__ == "__main__":
     start()
