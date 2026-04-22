26fb59e21b6e2f002f4da87cf7ec78660f6929a6
julia <gabrielassisgomes015@gmail>
2026-04-22 19:29:22 -0300

    Uma parte da interface

--- backend/interface.py
+++ backend/interface.py
@@ -1,6 +1,18 @@
 import tkinter as tk
 
 app = tk.Tk()
-app.title("BUSSOLA DIGITAL")
+app.title("BUSSOLA DIGITAL") 
 app.geometry("700x500")
+tk.Label(app, text="Guia digital").pack()
+ 
+def resposta_botao():
+    nome = entrada_texto.get()
+    app['text'] = f"Olá, {nome}"
+
+entrada_texto = tk.Entry(app)
+entrada_texto.pack()
+
+tk.Button(app, text="Segurança Digital", command=resposta_botao).pack()
+
+
 app.mainloop()
